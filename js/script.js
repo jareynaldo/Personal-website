8c3a7428eec4542862d68f237926cfed7c989d52
José Reynaldo <ja.reynaldo@outlook.com>
2024-06-13 23:13:25 -0400

    lots to go, but main problem being shuttle not changing from rocket

--- js/script.js
+++ js/script.js
@@ -1,23 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
     const rocket = document.getElementById('rocket');
-    const regularShuttle = document.createElement('img');
-    regularShuttle.src = '../Images/shuttle.png'; // Update with your actual image path
-    regularShuttle.id = 'regular-shuttle';
-    document.body.appendChild(regularShuttle);
-    
+    const regularShuttle = document.getElementById('regular-shuttle');
+
     window.addEventListener('scroll', () => {
         const scrollPosition = window.scrollY;
         if (scrollPosition > 10 && scrollPosition < 300) {
-            rocket.style.bottom = '-100%';
+            rocket.style.top = '50%';
             rocket.style.opacity = '1';
-            rocket.style.animationPlayState = 'running';
-        } else if (scrollPosition >= 200) {
+            regularShuttle.style.opacity = '0';
+        } else if (scrollPosition >= 300) {
             rocket.style.opacity = '0';
-            regularShuttle.style.top = '45%'; // Adjust this value based on where you want the regular shuttle to appear
+            rocket.style.top = '10%';
+            regularShuttle.style.top = '45%';
             regularShuttle.style.opacity = '1';
         } else {
             rocket.style.top = '50%';
-            rocket.style.opacity = '0';
+            rocket.style.opacity = '1';
             regularShuttle.style.opacity = '0';
         }
     });
